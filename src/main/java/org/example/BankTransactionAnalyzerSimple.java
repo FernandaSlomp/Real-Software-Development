bb64147e5d68d8e294461435a3de194d052092d8
Fernanda Slomp <fernandaslomp@hotmail.com>
2024-03-07 21:47:02 -0300

    add BankTransactionAnalyzerSimple

---
+++ src/main/java/org/example/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class BankTransactionAnalyzerSimple
+{
+    public static void main( String[] args ) {
+        System.out.println("hey");
+    }
+}
