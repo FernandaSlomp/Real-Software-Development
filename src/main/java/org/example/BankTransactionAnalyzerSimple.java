477b689befaa9452d681e6a0df21a02c6545cd93
Fernanda Slomp <fernandaslomp@hotmail.com>
2024-03-07 22:16:38 -0300

    create wrong methods to bank analyzer

--- src/main/java/org/example/BankTransactionAnalyzerSimple.java
+++ src/main/java/org/example/BankTransactionAnalyzerSimple.java
@@ -1,12 +1,64 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class BankTransactionAnalyzerSimple
 {
-    public static void main( String[] args ) {
-        System.out.println("hey");
+    private static final String RESOURCES = "src/main/resources/";
+
+    public static void main( String[] args ) throws IOException {
+
+        valorTotalTodosMeses();
+        valorTotalJaneiro();
+
+    }
+
+    private static void valorTotalTodosMeses() throws IOException {
+        final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
+        final List<String> lines = Files.readAllLines(path);
+
+        double valorTotal = 0;
+
+        for(final String line: lines){
+            String[] colunas = line.split(",");
+            double amount = Double.parseDouble(colunas[1]);
+            valorTotal += amount;
+        }
+
+        System.out.println("valorTotal " + valorTotal);
+
+    }
+
+    private static void valorTotalJaneiro() throws IOException {
+        final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
+        final List<String> lines = Files.readAllLines(path);
+
+        final DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        double valorTotalJan = 0;
+
+        for(final String line: lines){
+            String[] colunas = line.split(",");
+
+            LocalDate date = LocalDate.parse(colunas[0], formatterDate);
+
+            if(date.getMonth() == Month.JANUARY){
+                double amount = Double.parseDouble(colunas[1]);
+                valorTotalJan += amount;
+            }
+        }
+
+        System.out.println("valorTotalJan: " + valorTotalJan);
     }
 }
